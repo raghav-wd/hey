0f1201305b8e0912de8c72cf6dbc10acf9155d4f
raghav-wd <41248639+raghav-wd@users.noreply.github.com>
2020-05-27 01:07:26 +0530

    Updates

---
+++ precache-manifest.2a7fd80c0146f9f7f630df80bea18362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba37d58c3fce24119fc749c097566ebc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83aaa810ec7e0e3c5684",
+    "url": "/static/css/main.1158dd0a.chunk.css"
+  },
+  {
+    "revision": "6d244ff637b6a2c6d635",
+    "url": "/static/js/2.98abc9fd.chunk.js"
+  },
+  {
+    "revision": "c344ff0e273d3dd0833ad3a0d4c0857e",
+    "url": "/static/js/2.98abc9fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83aaa810ec7e0e3c5684",
+    "url": "/static/js/main.d9122024.chunk.js"
+  },
+  {
+    "revision": "bf38fd2ca016b7a722ee",
+    "url": "/static/js/runtime-main.9ec3ee04.js"
+  }
+]);
